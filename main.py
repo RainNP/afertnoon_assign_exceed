9c0e3990f97f004418a80fb2e37ae9c054264084
RainNP <99106894+RainNP@users.noreply.github.com>
2022-02-06 17:01:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
-from pymongo import MongoClient
+from xml.dom.minidom import Element
+from fastapi import FastAPI, Query, HTTPException
+from typing import Optional
 from pydantic import BaseModel
+from pymongo import MongoClient
+from fastapi.encoders import jsonable_encoder
 
 class Reservation(BaseModel):
     name : str
@@ -10,10 +13,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["afternoon_assign"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["table"]
 
 app = FastAPI()
 
@@ -21,21 +24,43 @@ app = FastAPI()
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    result = collection.find_one({"name":name},{"_id" :0})
+    print(result)
+    return {"result" : result}
 
-@app.get("reservation/by-table/{table}")
+@app.get("/reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    result = collection.find({"table_number" : table},{"_id": 0,"table_number":0})
+    myre = []
+    for r in result:
+        myre.append(r)
+    return {"result" : myre}
+
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    pass
+    check = collection.find_one({"time":reservation.time,"table_number" : reservation.table_number},{"_id":0})
+    if (check == None):
+        reservation = jsonable_encoder(reservation)
+        collection.insert_one(reservation)
+        return {"result" : "Finish"}
+    else:
+        return {"result" : "Cannot reserve"}
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
-    pass
+    check = collection.find_one({"time":reservation.time,"table_number" : reservation.table_number},{"_id":0})
+    if (check == None):
+        q = {"name":reservation.name}
+        new = {"$set" : {"time":reservation.time,"table_number" : reservation.table_number}}
+        collection.update_one(q, new)
+        return {"result" : "Finish"}
+    else:
+        return {"result" : "Cannot reserve"}
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    pass
+    q = {"name" : name, "table_number": table_number}
+    collection.delete_one(q)
+    return {"result" : "Finish"}
 
